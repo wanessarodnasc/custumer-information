6e93d3bb0e4afb9fe462971bb9985f0a60ef73c8
Wanessa Nascimento <wanessanasccimento@gmail.com>
2020-03-28 12:02:33 +0100

    Change configuration and add default informaion

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO custumer (id, cpf, complete_name, card_number, expired_date, code, is_avaliable_cpf_payment) VALUES
-  (1, '35177355223', 'Anita Garibaldi', '1234 5678 9101 1121', '01/23', '123', 0);
+  (1, '35177355221', 'Anita Garibaldi', '1234 5678 9101 1121', '01/23', '123', 1),
+  (2, '35177355222', 'Nicolau Copérnico', '1234 5678 9101 1122', '01/23', '123', 1),
+  (3, '35177355223', 'Elizabeth Alexandra Mary Windsor', '1234 5678 9101 1123', '01/23', '123', 0),
+  (4, '35177355224', 'Winston Churchill', '1234 5678 9101 1124', '01/87', '123', 0);
   
